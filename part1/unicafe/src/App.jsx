95a2a9189bca3a374ec2cae014afc15d93bcfc20
montionsanchez <montionproductions@gmail.com>
2025-02-23 20:50:13 -0600

    Unicafe 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -22,7 +22,10 @@ const App = () => {
       <h1>Statistics</h1>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p> 
+      <p>Bad: {bad}</p>
+      <p>All ratings: {good + neutral + bad}</p>
+      <p>Average: {(good - bad) / (good + neutral + bad)}</p>
+      <p>Positive: {good / (good + neutral + bad) * 100} %</p>
     </div>
   )
 }
