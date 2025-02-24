97b8040f10e658478bc792ef34cf7601f681aa4c
montionsanchez <montionproductions@gmail.com>
2025-02-23 23:58:11 -0600

    part 2.6 Phonebook

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,55 +1,49 @@
-import Course from "./components/Course"
+import { useState } from 'react'
+import Note from './components/Note'
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+
+  const addPhone = (event) => {
+    event.preventDefault()
+    const phoneObject = {
+      number: '000000000000',
+      name: newName,
+      id: persons.length + 1
     }
-  ]
+  
+    setPersons(persons.concat(phoneObject))
+    setNewName('')
+  }
 
+  const handleInputTextChange = (event) => {
+    setNewName(event.target.value)
+  }
 
-  return (<>
-    {courses.map(course => <Course key={course.id} course={course} />)}
-  </>)
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <form onSubmit={addPhone}>
+        <div>
+          name: <input 
+            value={newName}
+            onChange={handleInputTextChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => 
+        <p key={person.name}>{person.name}</p>
+      )}
+    </div>
+  )
 }
 
 export default App
