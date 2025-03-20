91131c25f2d960973008dbdeaadf20e0d8da043e
montionsanchez <montionproductions@gmail.com>
2025-03-19 18:28:28 -0600

    part 5.8

--- part5/bloglist-frontend-main/src/components/Blog.jsx
+++ part5/bloglist-frontend-main/src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [showAll, setShowAll] = useState(false)
+  const [buttonText, setButtonText] = useState('view')
+
+  const handleViewButton = (event) => {
+    event.preventDefault()
+    setShowAll(!showAll)
+    setButtonText(!showAll ? 'hide' : 'view')
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}<button onClick={handleViewButton}>{buttonText}</button>
+      </div>
+      {showAll && (
+        <div>
+          <p>Likes: {blog.likes}</p>
+          <p>Url: {blog.url}<button>like</button></p>
+          <p>Author: {blog.author}</p>
+        </div>
+      )}
+    </div>  
+  )
+}
 
 export default Blog
