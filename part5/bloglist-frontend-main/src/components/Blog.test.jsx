a13d0fb94ccaf929d6cbb5a9e6131a48d4fd2c1a
montionsanchez <montionproductions@gmail.com>
2025-03-25 18:11:39 -0600

    part 5.14

--- part5/bloglist-frontend-main/src/components/Blog.test.jsx
+++ part5/bloglist-frontend-main/src/components/Blog.test.jsx
@@ -4,7 +4,6 @@ import Blog from './Blog'
 import { describe, expect } from 'vitest'
 
 describe('blog tests', () => {
-test('Blog shows title and author', () => {
     const blog = {
         title: 'Test name',
         url: 'test.com',
@@ -16,16 +15,26 @@ test('Blog shows title and author', () => {
         username: 'Montion',
         token: 'testtoken'
     }
-    const { container } = render(<Blog blog={blog} user={userObj}/>)
 
-    const div = container.querySelector('.blog')
-    screen.debug(div)
-    expect(div).toHaveTextContent(
-        'Test name'
-    )
-    expect(div).toHaveTextContent(
-        'Montion'
-    )
-})
+    it("shows only title and author by default", () => {
+        render(<Blog blog={blog} user={userObj} />);
+    
+        expect(screen.getByText(/Test name/i)).toBeDefined();
+        expect(screen.getByText(/Montion/i)).toBeDefined();
+        expect(screen.queryByText("Likes: 69")).toBeNull();
+        expect(screen.queryByText("Url: test.com")).toBeNull();
+    });
+
+    it("shows details when the 'view' button is clicked", async () => {
+        render(<Blog blog={blog} user={userObj} />);
+        const user = userEvent.setup();
+
+        const viewButton = screen.getByText("view");
+        await user.click(viewButton);
+
+        screen.debug();
+        expect(screen.getByText("Likes: 69")).toBeDefined();
+        expect(screen.getByText("Url: test.com")).toBeDefined();
+    });
 
 })
